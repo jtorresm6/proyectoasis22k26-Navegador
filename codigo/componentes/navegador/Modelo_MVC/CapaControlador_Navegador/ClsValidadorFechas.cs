48632e35005e9910f7d0871f86333007a58854d8
Sofía de la Rosa <sdelarosaj@miumg.edu.gt>
2026-09-10 10:28:09 -0600

    feat: validación de fechas en ingreso de empleados

---
+++ codigo/componentes/navegador/Modelo_MVC/CapaControlador_Navegador/ClsValidadorFechas.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_Controlador_Navegador
+{
+    public class ClsValidadorFechas
+    {
+        public string NavegadorMetValidarFechas(
+            DateTime FechaNacimiento,
+            DateTime FechaContratacion)
+        {
+            DateTime FechaActual = DateTime.Today;
+
+            if (FechaNacimiento > FechaActual)
+            {
+                return "La fecha de nacimiento no puede ser futura.";
+            }
+
+            if (FechaContratacion > FechaActual)
+            {
+                return "La fecha de contratación no puede ser futura.";
+            }
+
+            if (FechaNacimiento >= FechaContratacion)
+            {
+                return "La fecha de nacimiento debe ser anterior a la fecha de contratación.";
+            }
+
+            return null;
+        }
+    }
+}
